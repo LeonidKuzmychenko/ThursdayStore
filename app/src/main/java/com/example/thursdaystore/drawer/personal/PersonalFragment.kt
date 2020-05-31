d612f3dacd5881d9c7ec43423d1e39ce1e4164ca
Kuzmychenko <leonid.kuzmychenko@ab-soft.net>
2020-05-31 23:27:46 +0300

    personal page

--- app/src/main/java/com/example/thursdaystore/drawer/personal/PersonalFragment.kt
+++ app/src/main/java/com/example/thursdaystore/drawer/personal/PersonalFragment.kt
@@ -1,12 +1,9 @@
 package com.example.thursdaystore.drawer.personal
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.thursdaystore.R
 
@@ -15,10 +12,20 @@ class PersonalFragment : Fragment() {
     private lateinit var personalViewModel: PersonalViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
         personalViewModel = ViewModelProvider(this).get(PersonalViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_personal, container, false)
-        val textView: TextView = root.findViewById(R.id.text_personal)
-        personalViewModel.text.observe(this, Observer { textView.text = it })
-        return root
+        return inflater.inflate(R.layout.fragment_personal, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment_person_toolbar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.person_ok) {
+            Toast.makeText(context,"SAVE", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
